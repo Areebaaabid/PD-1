f0ada6ba1c4fd522284d9c7023efd538621eab2d
Areebaaabid <144439266+Areebaaabid@users.noreply.github.com>
2023-09-17 00:11:10 -0700

    Add files via upload

---
+++ Task09.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+main(){
+	cout << "  --------------------------------                               " <<endl;
+	cout << "                                                                 " <<endl;
+	cout << "        o       ^__^                                             " <<endl;
+	cout << "         o      (oo)\\__________                                 " <<endl;                                             
+	cout << "                                                                 " <<endl;
+	cout << "                (__)\\          )\\/\\                           " <<endl;
+	cout << "                     ||------W  |                                " <<endl;
+	cout << "                     ||        ||                                " <<endl;
+	
+
+
+
+
+
+}
---
+++ Task13.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+main(){
+	system("color 01");
+	cout << "     +--^---------- ------- ----- ---------- --^-       " << endl;
+	cout << "                   '       '     '          '     '     " << endl;
+	cout << "      |  |||||||||   '-------'    |               O     " << endl;
+	cout << "      '+--------------------------^---------------|     " << endl;
+	cout << "        '                                               " << endl;
+	cout << "           --------- ---------- ----------------'       " << endl;
+	cout << "          '         '          '                        " << endl;
+	cout << "            /  XXXXXX  /'|      /'                      " << endl;
+	cout << "           /  XXXXXX  /  |     /'                       " << endl;
+	cout << "          /  XXXXXX  /'-------'                         " << endl;
+	cout << "         /  XXXXXX  /                                   " << endl;
+	cout << "        /  XXXXXX  /                                    " << endl;
+	cout << "       (-----------(                                    " << endl;
+	cout << "         '--------'                                     " << endl;
+
+
+
+
+
+
+
+
+}
